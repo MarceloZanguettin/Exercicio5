a28a7c0e2e511d401d756f7d9415b929b06bac02
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-03-14 17:08:22 +0000

    Entregue

--- Exercicio5.java
+++ Exercicio5.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 public class Exercicio5{
 
     public static void main(String[] args){
-        int valor;
-        int x;
+        float valor;
+        float x;
 
         Scanner leitura = new Scanner(System.in);
 
         System.out.println("Digite um valor inteiro: ");
-        valor=leitura.nextInt();
-
-        if (valor%2 == 0){
-            x = valor/2;
-            System.out.println(x);
-        } else {
-            x = (3*valor)+1;
-            System.out.println(x);
+        valor=leitura.nextFloat();
+        x = valor;
+        while (x>1){
+            if (valor%2 == 0){
+                x = valor/2;
+                valor = x;
+                System.out.println(x+"-> ");
+            } else {
+                x = (3*valor)+1;
+                valor = x;
+                System.out.println(x+"-> ");
+            }
         }
         leitura.close();
     }
